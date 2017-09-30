c6604bafaf34634c2fc10117373775eb5fe7b431
Claudemir <claudemir.bezerraguinho@gmail.com>
2017-09-30 19:13:57 -0400

    adicionando mais projetos

---
+++ AprendendoAngular.UI.AngularJs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AprendendoAngular.UI.AngularJs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
